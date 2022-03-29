0698d3481797b485758a10358bde35da3a594465
Lyndsey Coombs <lyndscoombs@aol.com>
2022-03-29 16:51:52 +0100

    code done

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const { joinNames } = require(".");
+
+const numberToReversedDigits = number => {
+//    return (number.toString().split('').reverse());
+    const numbers = (number.toString().split('').reverse());
+    return numbers.map(str => +str);
+};
 
 module.exports = numberToReversedDigits;
