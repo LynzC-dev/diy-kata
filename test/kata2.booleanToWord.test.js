bd3633ae32396965eedd0ecfdd59df262f370927
Lyndsey Coombs <lyndscoombs@aol.com>
2022-03-29 11:19:00 +0100

    Fizz Buzz & Boolean done

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -2,4 +2,6 @@ const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
   // how do we create specs again???
+  expect(booleanToWord(true)).toBe('Yes')
+  expect(booleanToWord(false)).toBe('No')
 });
