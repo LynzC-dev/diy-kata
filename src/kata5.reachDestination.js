117f745346e0b1912f1bb3a3c426ee259267a43e
Lyndsey Coombs <lyndscoombs@aol.com>
2022-03-30 11:03:33 +0100

    ReachDestination Complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,11 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+    // const time = Math.round(((distance / speed)/0.5)* 0.5) + 0.5;
+let time = (distance / speed);
+    if (time -Math.floor(time)<0.5)
+    time = Math.floor(time) + 0.5;
+    if (time-Math.floor(time) >0.5)
+    time = Math.ceil(time);
+return `I should be there in ${time} hours.`;
+};
+  
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(88,10)).toEqual("I should be there in 9 hours.")
+    expect(reachDestination(44,10)).toEqual("I should be there in 4.5 hours.")
+    expect(reachDestination(22,10)).toEqual("I should be there in 2.5 hours.")
+  });
 });
